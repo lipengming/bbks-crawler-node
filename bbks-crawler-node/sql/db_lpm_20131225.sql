eaa3f2ddd36c1ed7a72b8bf62b5f84e281875e3b
cncduLee <shouli1990@gmail.com>
2013-12-25 16:13:09 +0800

    add sql

---
+++ bbks-crawler-node/sql/db_lpm_20131225.sql
@@ -0,0 +1,14 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.0.22-community-nt - MySQL Community Edition (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL version:             7.0.0.4053
+-- Date/time:                    2013-12-25 16:11:29
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+-- Data exporting was unselected.
+/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
